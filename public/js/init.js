ce1d79748d7d8849cdce1a4ae5d94842d5b38335
Noel Aby Das <noelabydas@gmail.com>
2018-12-16 19:50:46 +0530

    add responsive fields

--- public/js/init.js
+++ public/js/init.js
@@ -1,6 +1,6 @@
  $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 45 // Creates a dropdown of 15 years to control year
   });
 
 
